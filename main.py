92c4dc88e8c3425982d5689ff02f0ad11721a27e
Patrick Lucas <me@patricklucas.com>
2015-03-01 17:26:09 -0800

    Improvements

--- main.py
+++ main.py
@@ -1,26 +1,74 @@
 from collections import namedtuple
+import json
 
 from consul import Consul
 from flask import Flask, render_template, redirect, url_for, request, abort
 
-PREFIX = 'service/uzblmonitor'
+PREFIX = 'service/uzblmonitor/'
 
 HostConfig = namedtuple('HostConfig', "host url")
 
 app = Flask('uzblmonitor-ui')
-c = Consul()
+
+
+class VK(Consul.KV):
+
+    def keys(
+            self,
+            prefix,
+            separator=None,
+            token=None,
+            dc=None):
+        assert not prefix.startswith('/')
+        params = {}
+        params['keys'] = 1
+        if separator:
+            params['separator'] = separator
+        token = token or self.agent.token
+        if token:
+            params['token'] = token
+        dc = dc or self.agent.dc
+        if dc:
+            params['dc'] = dc
+
+        def callback(response):
+            if response.code == 404:
+                data = None
+            else:
+                data = json.loads(response.body)
+            return response.headers['X-Consul-Index'], data
+
+        return self.agent.http.get(
+            callback, '/v1/kv/%s' % prefix, params=params)
+
+
+class Lusnoc(Consul):
+
+    KV = VK
+
+    def __init__(self, *args, **kwargs):
+        super(Lusnoc, self).__init__(*args, **kwargs)
+        self.kv = self.KV(self)
+
+
+c = Lusnoc('172.16.10.13')
+
+def mk_key(*pieces):
+    return PREFIX + '/'.join(pieces)
+
+
+def get_hosts():
+    _, keys = c.kv.keys(mk_key('hosts/'), separator='/')
+    return [k.rsplit('/', 2)[-2] for k in keys]
 
 
 def get_host_configs():
-    _, values = c.kv.get(PREFIX, recurse=True)
+    _, objs = c.kv.get(mk_key('hosts/'), recurse=True)
 
     host_configs = []
-    for v in values:
-        host = v['Key'][len(PREFIX)+1:].split('/', 1)[0]
-        if host == 'auth':
-            continue
-
-        url = v['Value']
+    for o in objs or []:
+        host = o['Key'][len(PREFIX):].split('/', 2)[1]
+        url = o['Value']
 
         host_configs.append(HostConfig(host, url))
 
@@ -45,14 +93,15 @@ def host():
     if not all([host, url, submit]):
         return abort(400)
 
-    key = "{0}/{1}/url".format(PREFIX, host)
+    key = mk_key('hosts', host, 'url')
 
     if submit in ('Save', 'Create'):
         c.kv.put(key, url)
     elif submit == 'Delete':
         c.kv.delete(key)
 
-    return redirect(url_for('hosts'))
+    return redirect(url_for('home'))
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
